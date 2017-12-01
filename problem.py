2a402916fd82842625fb1eb3a4ee0451beee7806
Nathaniel Lahn <lahnn@vt.edu>
2017-12-01 14:22:54 -0500

    Added csv generation. See output_all.csv for the full set. Some problems encountered errors while parsing. See BadDataList.txt

--- problem.py
+++ problem.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import util
 
+
+"""
+Note, input path must be of the form
+"texts/<contest #>/<Problem Letter String>.html"
+
+See bottom of file for examples.
+"""
+
 class Problem:
     def __init__(self, file_path):
         if file_path.endswith(".html"):
@@ -15,9 +23,11 @@ class Problem:
 
         page_soup = BeautifulSoup(raw_html, "lxml")
 
+        #very sensitive to input format...
         path_items = path.split("/")
-        self.contest = path_items[1]
-        self.letter = path_items[2].split(".")[0]
+        self.contest = path_items[2]
+        self.letter = path_items[3].split(".")[0]
+
         self.title = page_soup.find(attrs={'class': 'title'}).text
         self.time_limit = page_soup.find(
             attrs={'class': 'time-limit'}).text.split('time limit per test')[1]
@@ -32,14 +42,37 @@ class Problem:
 
         tag_boxes = page_soup.findAll(attrs={'class': 'tag-box'})
         self.tags = []
+
         for tag_box in tag_boxes:
             self.tags.append(tag_box.text.strip())
+
+        #Sanitize the fields
+
+        self.contest = self.sanitize(self.contest)
+        self.letter = self.sanitize(self.letter)
+        self.title = self.sanitize(self.title)
+        self.time_limit = self.sanitize(self.time_limit)
+        self.memory_limit = self.sanitize(self.memory_limit)
+        self.main_text = self.sanitize(self.main_text)
+        self.input_specification = self.sanitize(self.input_specification)
+        self.output_specification = self.sanitize(self.output_specification)
+        for i in range(len(self.tags)):
+            self.tags[i] = self.sanitize(self.tags[i])
 
         #Later, may want to add sample tests, and Notes to this class, but it is difficult to parse the samples
         # in a useful fashion.
         #self.sample_tests = page_soup.find(attrs={'class': "sample-test"}).findAll
         #print(self.sample_tests)
 
+    """
+    Currently, removes all chars to make it alpha-numeric with spaces and hyphens
+    """
+    def sanitize(self, string):
+        #Remove unwanted chars
+        raw =  ''.join([i if ord(i) < 128 and (i.isalnum() or i.isspace() or i == '-') else ' ' for i in string])
+
+        #Condense extra whitespace
+        return ' '.join(raw.split())
 
     def parse_pickle(self, path):
         pass
@@ -48,15 +81,19 @@ class Problem:
     def __str__(self):
         return util.stringify(self)
 
+
+
 if __name__ == '__main__':
     #basic test. Load a single file. No tags
-    problem = Problem("texts/889/E.html")
-    print(problem)
-
-    #single problem with multiple tags
-    problem = Problem("texts/888/G.html")
+    problem = Problem("./texts/889/E.html")
     print(problem)
 
     #single problem with multiple tags
     problem = Problem("./texts/888/G.html")
     print(problem)
+
+    #Some problems with bad formats. All problems from comp 524 should be ignored
+    #problem = Problem("./texts/524/A.html")
+
+    problem = Problem("./texts/711/E.html")
+    print problem
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
-#Canabalized from https://stackoverflow.com/questions/32910096/is-there-a-way-to-auto-generate-a-str-implementation-in-python
+
 import os
 import problem
 import random
 
+#Canabalized from https://stackoverflow.com/questions/32910096/is-there-a-way-to-auto-generate-a-str-implementation-in-python
 def stringify(thingy):
     attributes = dir(thingy)
     res = thingy.__class__.__name__ + "(\n"
@@ -18,10 +19,58 @@ def stringify(thingy):
     res += ")"
     return res
 
+def CSVHeaderString(problem):
+    attributes = dir(problem)
+    headers = []
+    for attr in attributes:
+        if attr.startswith("__") and attr.endswith("__"):
+            continue
+        if callable(getattr(problem, attr)):
+            continue
+        headers.append(attr)
+    return ",".join(headers)
+
+
+
+def CSVItemString(problem):
+    attributes = dir(problem)
+    headers = []
+    for attr in attributes:
+        if attr.startswith("__") and attr.endswith("__"):
+            continue
+        if callable(getattr(problem, attr)):
+            continue
+        #headers.append(problem.sanitize(str(getattr(problem, attr))))
+        headers.append((str(getattr(problem, attr))).replace(",", "/"))
+    return ",".join(headers)
+
+
+"""Creates and writes a csv file containing all the given problem info
+First line of csv will have header info
+CSV fields will simply have strings of the format given by str(field)"""
+
+def outputCSV(problems, text_path):
+    if (len(problems) == 0):
+        print("Error, empty list sent as input to outputCSV function")
+        return ""
+    with open(text_path, 'wb') as outfile:
+        header = (CSVHeaderString(problems[0]) + "\n")
+        outfile.write(header.encode('utf8'))
+        for problem in problems:
+            line = CSVItemString(problem) + "\n"
+            outfile.write(line.encode('utf8'))
+
 """
 Return list of Problem class objects. Roughly 4000
 """
 def loadAllProblems():
+    return loadNProblems(10**10)
+
+"""
+Same as above, but only load some small number of problems. Will load all problems if n exceeds total number.
+"""
+def loadNProblems(n):
+    count = 0
     root_dir = '.'
     print("Loading all problems...")
     problems = []
@@ -31,8 +80,18 @@ def loadAllProblems():
             test_dir = ".\\texts"
             if name.startswith(test_dir) and name.endswith(".html"):
                 refined_name = name.replace("\\", "/")
-                problems.append(problem.Problem(refined_name))
+                try:
+                    problems.append(problem.Problem(refined_name))
+                except:
+                    print("problem {} had a formatting issue:  {}".format(count, name))
+                count += 1
+                """Progress counter"""
+                print("problem {} loaded:  {}".format(count, name))
+                if count >= n:
+                    return problems
     return problems
+
+
 
 if __name__ == '__main__':
     import time
@@ -40,7 +99,11 @@ if __name__ == '__main__':
 
     problems = loadAllProblems()
     print("{} problems loaded".format(len(problems)))
+
     for i in range(5):
         sam = random.choice(problems)
         print(sam)
+
+    outputCSV(problems, "output_all.csv")
+
     print(time.clock() - start_time, "seconds")
